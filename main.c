2757a4897cdebf25598b170571c124a8dd2cbcb5
alexander.neumeister@gmail.com <alexander.neumeister@gmail.com>
2023-10-11 23:39:31 +0200

    fast fertig

--- main.c
+++ main.c
@@ -2,38 +2,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+HWND selbst;
+char titel[300];
 
 int FindWindowText(char *txt)
 {
     HWND hwnd;
     int treffer = 0;
-    static hStr[300];
+    static char hStr[300];
     
     printf("Suche: %s\n",txt);
     
     hwnd = FindWindow(NULL, NULL);
-    
+
     while(hwnd != NULL)
     {
-        //TODO - Fenster suchen
-        GetWindowText(hwnd, hStr, 300);
-        if (strstr(hStr,txt) != NULL)
+        if (hwnd != selbst)
         {
-            printf("gefunden: %s\n", hStr);
-            treffer++;
+            GetWindowText(hwnd, hStr, 300);
+             if (strstr(hStr,txt) != NULL)
+            {
+                printf("gefunden: %s\n", hStr);
+                treffer++;
+            }
         }
-        hwnd = GetWindow(hwnd,GW_HWNDNEXT);
+        hwnd = GetWindow(hwnd,GW_HWNDNEXT);            
     }
+    SetConsoleTitle(titel);
+    
     return treffer;
 }
 
 int main(int argc, char *argv[]) 
 {
+    selbst = GetConsoleWindow();
+    
+    GetConsoleTitle(titel, 300);
+    // printf("titel: %s\n",titel);
+    
     if (argc == 2)
     {
+        SetConsoleTitle("?");
+        Sleep(100);
+
+        
         return FindWindowText(argv[1]);
     }
-    printf("Verwendung: ChkWnd <Fenstertitel>\n%ERRORLEVEL% ist Anzahl Treffer\n");
+    printf("Verwendung: ChkWnd <Fenstertitel>\n%%ERRORLEVEL%% ist Anzahl Treffer\n");
 	return 0;
 }
