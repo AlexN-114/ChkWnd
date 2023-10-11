debb0e3834b0665dfb284d6ed7beda9989760c25
alexander.neumeister@gmail.com <alexander.neumeister@gmail.com>
2023-10-11 23:37:12 +0200

    fast fertig

---
+++ ChkWnd_private.h
@@ -0,0 +1,23 @@
+/* THIS FILE WILL BE OVERWRITTEN BY Red Panda C++ */
+/* DO NOT EDIT ! */
+
+#ifndef ChkWnd_private_rc
+#define ChkWnd_private_rc
+
+/* VERSION DEFINITIONS */
+#define VER_STRING	"1.0.0.42"
+#define VER_MAJOR	1
+#define VER_MINOR	0
+#define VER_RELEASE	0
+#define VER_BUILD	42
+#define COMPANY_NAME	"//-\\. //\\//. - SW"
+#define FILE_VERSION	"1.0.0.0"
+#define FILE_DESCRIPTION	"Suche Fenster mit bestimmten Text im Titel"
+#define INTERNAL_NAME	"Check Window"
+#define LEGAL_COPYRIGHT	"(c) - 2023"
+#define LEGAL_TRADEMARKS	""
+#define ORIGINAL_FILENAME	"ChkWnd.exe"
+#define PRODUCT_NAME	"ChkWnd"
+#define PRODUCT_VERSION	"1.0.0.0"
+
+#endif /*ChkWnd_private_rc*/
